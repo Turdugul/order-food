c759201f032d694f9b3b31d00083101aba33cc6e
Turdugul <okonbaevaturdugul@gmail.com>
2023-08-01 12:45:16 +0100

    add ui modal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Header } from "./components/header/Header";
 import { MealsSummary } from "./components/meals-summary/MealsSummary";
 import { Meals } from "./components/meals/Meals";
+
 function App() {
   return (
     <div>
       <Header />
       <MealsSummary/>
       <Meals />
+   
      
     </div>
   );
